e82203f5292af0215ece2559f0329760c4fc67c2
POC-Client-AngularJS <poc.client.angularjs@gmail.com>
2020-06-04 21:09:37 +0530

    Initial commit.

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function processForm(e) {
+  if (e.preventDefault) e.preventDefault();
+  /*
+   * Perform operations on form submit.
+   */
+  let strSampleString = document.getElementById("idInpSampleString").value;
+  document.getElementById("idSpanReversedString").innerHTML = reverseString1(
+    strSampleString
+  );
+  /*
+   * You must return "false" to prevent the default form behavior.
+   */
+  return false;
+}
+
+var form = document.getElementById("idFormStringReverse");
+if (form.attachEvent) {
+  form.attachEvent("submit", processForm);
+} else {
+  form.addEventListener("submit", processForm);
+}
